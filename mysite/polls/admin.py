a81349e94c38e812777e1260d3d7b6846ad25e24
Jason Morrison <jmorrison@minervaproject.com>
2017-11-30 14:12:04 -0800

    Test app for nested-inline-test and django-1.11

---
+++ mysite/polls/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+from .models import *
+
+class LevelThreeInline(NestedStackedInline):
+    model = LevelThree
+    extra = 1
+    fk_name = 'level'
+
+
+class LevelTwoInline(NestedStackedInline):
+    model = LevelTwo
+    extra = 1
+    fk_name = 'level'
+    inlines = [LevelThreeInline]
+
+
+class LevelOneInline(NestedStackedInline):
+    model = LevelOne
+    extra = 1
+    fk_name = 'level'
+    inlines = [LevelTwoInline]
+
+
+class TopLevelAdmin(NestedModelAdmin):
+    model = TopLevel
+    inlines = [LevelOneInline]
+
+
+admin.site.register(TopLevel, TopLevelAdmin)
+admin.site.register(LevelOne)
